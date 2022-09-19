06cf9568fdd473bcd2e377f4894f8b57cc5e0f9b
Venktesh B <venktesh2002@gmail.com>
2022-09-19 19:48:13 +0530

    Assignment 2

---
+++ Assignments/Team Lead/Assignment2/application.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+Appli = Flask(__name__)
+
+
+@Appli.route('/login')
+def login():
+    return render_template('html/login.html')
+
+
+@Appli.route('/Reg')
+def Registration():
+    return render_template('html/Registration.html')
+
+
+@Appli.route('/About')
+def about():
+    return render_template('html/About.html')
+
+
+if __name__ == '__main__':
+    Appli.run()
