e4dd73c5388ae854fb147deda7328a7fc642c3a8
Venktesh B <venktesh2002@gmail.com>
2022-11-20 00:42:27 +0530

    updation

---
+++ Project Development Phase/sprint4/MobileApplication/flask_api_connection.java
@@ -0,0 +1,12 @@
+package com.example.conzon;
+
+import okhttp3.OkHttpClient;
+
+public class flask_api_connection {
+    OkHttpClient okhttpclient = new OkHttpClient();
+
+    public void connection() {
+
+    }
+
+}
